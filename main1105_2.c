e383d504317a16cdd6a85ad561584c8227db171a
Ekin Ali Karahan <146540909+EkinAliKarahan@users.noreply.github.com>
2023-12-24 02:14:39 +0300

    Add files via upload

---
+++ main1105_2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    float oncekiTeklif;
+    float teklif;
+    int tur;
+} Artirma;
+
+int main(int argc, char *argv[]) {
+    Artirma artirma;
+    artirma.oncekiTeklif = -1;
+    artirma.tur = 0;
+    char c;
+
+    printf ("1969 model Ford Mustang Boss 429 acik artirmasina hos geldiniz\n");
+
+    for (artirma.tur = 1 ;; artirma.tur++) {
+        printf("\n %d. teklif (Bin Dolar) : \n \n", artirma.tur);
+
+                            if (scanf("%f", &artirma.teklif) != 1) {
+                                  printf("\n Yanlis deger, lutfen bir sayi giriniz! \n");
+                                    while ((c = getchar()) != '\n' && c != EOF)
+                                   ;
+                                   continue;
+                            }
+                             
+        if (artirma.teklif < artirma.oncekiTeklif) {
+            printf("\nKazanan %.1f Bin Dolar'lik teklifin sahibi oluyor\n", artirma.oncekiTeklif);
+            break;
+        }
+
+        artirma.oncekiTeklif = artirma.teklif ;
+    }
+
+    return 0;
+}
+
